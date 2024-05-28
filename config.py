70b2e768ec62bfd4a7333bbdd5aefb38a67bdfcf
Rasmus W. Lyngsøe <94936467+RALLERGUMSALLA@users.noreply.github.com>
2024-05-28 15:38:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://rasmuslogin:password@localhost/movie_site')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://rasmuslogin:password@localhost/DIS_project')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
